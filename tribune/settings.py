faca2319fa54bc10b5382f5797fcce399c225e7d
MugeraH <bwosihughes24@gmail.com>
2021-05-20 07:38:01 +0300

    add login and register forms

--- tribune/settings.py
+++ tribune/settings.py
@@ -33,14 +33,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+     'news',
+     'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'news',
-     'bootstrap4',
+   
      
 ]
 
--- tribune/urls.py
+++ tribune/urls.py
@@ -1,11 +1,24 @@
 
 from django.contrib import admin
 from django.urls import path,include,re_path
+import django.contrib.auth.urls 
+from django_registration.backends.one_step.views import RegistrationView
+from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
    path('admin/', admin.site.urls),
     re_path(r'',include('news.urls')),
-    re_path(r'^accounts/', include('registration.backends.simple.urls')),
+    
+    path('accounts/register/',
+        RegistrationView.as_view(success_url='/'),
+        name='django_registration_register'),
+
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('logout/', LogoutView.as_view(), {"next_page": '/login'}),
+    # re_path(r'^logout/$', 'django.contrib.auth.views.logout',{'next_page':'/login'})
+    # path('tinymce/', include('tinymce.urls'))
+  
 ]
 
